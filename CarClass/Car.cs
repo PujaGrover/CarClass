5c34ae94dcd1bcaef7ac40906162b8fe6f92dfe9
Puja Grover <puja_grover@yahoo.com>
2019-03-19 22:25:37 -0500

    Updated Code for car class Week* HW

---
+++ CarClass/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarClass
+{
+    public class Car
+    {
+        public float gasLevel;
+        //public float GasLevel { get; set; }
+        //float f = 13.0f;
+        //decimal d = 13.0m;
+
+        public float GasLevel
+        {
+            get
+            {
+                return gasLevel;
+            }
+            set
+            {
+                if (value < 0 || value > 13.0f)
+                {
+                    throw new FormatException();
+                }
+                else
+                {
+                    gasLevel = value;
+                }
+
+            }
+        }
+        public void AddGas(float gasIntake)
+        {
+            GasLevel += gasIntake;
+
+        }
+        public float FillUp()
+        {
+            float gasAdded = 13 - GasLevel;
+            GasLevel = 13;
+
+            return gasAdded;
+
+        }
+
+        public Car()
+        {
+            Random rand = new Random();
+            GasLevel = rand.Next(0, 14);
+
+        }
+    }
+}
--- CarClass/Program.cs
+++ CarClass/Program.cs
@@ -6,7 +6,17 @@ namespace CarClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+                //Console.WriteLine("Hello World!");
+                Car car = new Car();
+                car.GasLevel = 3.60f;
+
+                //car.AddGas(10);
+
+                Console.WriteLine("Gas Added: " + car.FillUp() + " units");
+                Console.ReadLine();
+
         }
+
     }
+
 }
